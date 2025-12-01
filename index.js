ea90e77edd3f6685d1329ca36a95504c9a1f2fa6
Egor Docenko <egordocenko359@gmail.com>
2025-12-01 12:04:50 +0300

    shjkdfhwejcxvb

--- index.js
+++ index.js
@@ -2,32 +2,31 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const sequelize = require("./config/db");
-const photosRouter = require("./routes/router.js");
+const photosRouter = require("./routes/router");
 const bot = require("./bot/bot");
 require("dotenv").config();
 
 const app = express();
 
 const allowedOrigins = ["https://bandana-dance.ru"];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (!allowedOrigins.includes(origin)) {
+      const msg = `CORS ошибка: ${origin} не разрешён`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+}));
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = `CORS ошибка: ${origin} не разрешён`;
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true,
-  })
-);
 app.use(express.json());
+
 const uploadsPath = path.resolve(__dirname, "uploads");
 console.log("Обслуживаю статику из:", uploadsPath);
 app.use("/uploads", express.static(uploadsPath));
+
 app.use("/api", photosRouter);
 
 const start = async () => {
@@ -39,7 +38,8 @@ const start = async () => {
     await sequelize.sync({ alter: process.env.NODE_ENV !== "production" });
     console.log("Таблицы синхронизированы");
 
-    bot.launch().then(() => console.log("Бот запущен"));
+    await bot.launch();
+    console.log("Бот запущен");
 
     const PORT = process.env.PORT || 3001;
     app.listen(PORT, () => {
@@ -47,14 +47,6 @@ const start = async () => {
     });
   } catch (err) {
     console.error("Сервер не запустился:", err.message);
-
-    if (err.name?.includes("Sequelize")) {
-      console.error("\nПроверь:");
-      console.error("  • sudo systemctl status postgresql");
-      console.error("  • Правильные ли данные в .env?");
-      console.error("  • Создан ли пользователь БД?");
-    }
-
     process.exit(1);
   }
 };
