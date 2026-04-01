5f4e7ffb13f3a05b1d54ec8ec9adab5fa986f0fd
EGOR-9527 <egordocenko359@gmail.com>
2026-04-01 21:11:24 +0300

    ujwehrkhjvassaioge

--- bot/utils/messageMonitor.js
+++ bot/utils/messageMonitor.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const axios = require("axios");
 const fs = require("fs").promises;
 const path = require("path");
+const FormData = require("form-data");
 
 const BASE_URL = `https://api.telegram.org/bot${process.env.PRO_TOKEN}`;
 
